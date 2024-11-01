9a52344d95a89c1bb11dd65d2b379b5a857e5466
Ovett Mora <ovettmora17@gmail.com>
2024-10-31 21:56:41 -0600

    feat: add Student entity

---
+++ src/main/java/org/demo/academicsystem/entity/Student.java
@@ -0,0 +1,39 @@
+package org.demo.academicsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+@EntityListeners(AuditingEntityListener.class)
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String address;
+
+    private Integer phone;
+}
