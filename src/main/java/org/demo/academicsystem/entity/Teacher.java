3fd36bdfa6239dfb89ac7f29a2978f27db23f432
Ovett Mora <ovettmora17@gmail.com>
2024-10-31 21:48:18 -0600

    feat: add Teacher entity

---
+++ src/main/java/org/demo/academicsystem/entity/Teacher.java
@@ -0,0 +1,40 @@
+package org.demo.academicsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "teachers")
+@EntityListeners(AuditingEntityListener.class)
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private Integer phone;
+    
+    private LocalDate dateOfBirth;
+    
+    private LocalDate dateOfJoining;
+    
+    private String department;
+    
+    private String specialization;
+}
