14ba324c0c6f34b03a176c2af985df1fc3b5fb1d
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 13:14:32 -0600

    feature: enhance CourseServiceImpl to retrieve all courses with mapping

--- src/main/java/org/demo/academicsystem/service/impl/CourseServiceImpl.java
+++ src/main/java/org/demo/academicsystem/service/impl/CourseServiceImpl.java
@@ -3,6 +3,9 @@ package org.demo.academicsystem.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.demo.academicsystem.dto.course.CourseRequest;
 import org.demo.academicsystem.dto.course.CourseResponse;
+import org.demo.academicsystem.entity.Course;
+import org.demo.academicsystem.mapper.CourseMapper;
+import org.demo.academicsystem.repository.CourseRepository;
 import org.demo.academicsystem.service.CourseService;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +14,16 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
+
     @Override
     public List<CourseResponse> getAllCourses() {
-        return List.of();
+        List<Course> courses = courseRepository.findAll();
+        return courses.stream()
+                .map(courseMapper::toResponse)
+                .toList();
     }
 
     @Override
