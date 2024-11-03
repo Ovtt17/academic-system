1778765f7020396017d86372ef72769b10bad864
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 12:44:13 -0600

    feature: implement getAllAssignments method in AssignmentController to retrieve all assignments

--- src/main/java/org/demo/academicsystem/controller/AssignmentController.java
+++ src/main/java/org/demo/academicsystem/controller/AssignmentController.java
@@ -1,13 +1,25 @@
 package org.demo.academicsystem.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.demo.academicsystem.dto.assignment.AssignmentResponse;
+import org.demo.academicsystem.service.AssignmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/assignments")
 @RequiredArgsConstructor
 @Tag(name = "Assignments")
 public class AssignmentController {
+    private final AssignmentService assignmentService;
+
+    @GetMapping
+    public ResponseEntity<List<AssignmentResponse>> getAllAssignments() {
+        return ResponseEntity.ok(assignmentService.getAllAssignments());
+    }
 }
