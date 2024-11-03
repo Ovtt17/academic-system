4c532a91b65e558ebdb4642ed6340dff966a6258
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 13:30:50 -0600

    feature: implement getCourseById method in CourseController with validation

--- src/main/java/org/demo/academicsystem/controller/CourseController.java
+++ src/main/java/org/demo/academicsystem/controller/CourseController.java
@@ -2,11 +2,13 @@ package org.demo.academicsystem.controller;
 
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.demo.academicsystem.dto.course.CourseResponse;
 import org.demo.academicsystem.service.CourseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<List<CourseResponse>> getAllCourses() {
         return ResponseEntity.ok(courseService.getAllCourses());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseResponse> getCourseById(@PathVariable @Valid Long id) {
+        return ResponseEntity.ok(courseService.getCourseById(id));
     }
 }
