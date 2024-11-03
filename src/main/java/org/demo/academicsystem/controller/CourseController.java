5fcc0014b26ac6ca10e7a700784e57c1419bcd25
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 13:19:48 -0600

    feature: implement getAllCourses method in CourseController

--- src/main/java/org/demo/academicsystem/controller/CourseController.java
+++ src/main/java/org/demo/academicsystem/controller/CourseController.java
@@ -3,12 +3,24 @@ package org.demo.academicsystem.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.demo.academicsystem.dto.course.CourseResponse;
+import org.demo.academicsystem.service.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
 @RequiredArgsConstructor
 @Tag(name = "Courses")
 public class CourseController {
+    private final CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
 }
