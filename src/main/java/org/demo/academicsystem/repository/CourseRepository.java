258804e84091d4a0dcc75c2bc6170f284dab1143
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 13:13:29 -0600

    refactor: rename ClassRepository to CourseRepository

--- src/main/java/org/demo/academicsystem/repository/CourseRepository.java
+++ src/main/java/org/demo/academicsystem/repository/CourseRepository.java
@@ -3,5 +3,5 @@ package org.demo.academicsystem.repository;
 import org.demo.academicsystem.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassRepository extends JpaRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
