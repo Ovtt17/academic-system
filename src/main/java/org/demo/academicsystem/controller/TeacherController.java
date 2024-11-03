43870323bfcc26319005346092d52f698c2c3657
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 13:08:30 -0600

    feature: implement TeacherController methods for CRUD operations on teachers

--- src/main/java/org/demo/academicsystem/controller/TeacherController.java
+++ src/main/java/org/demo/academicsystem/controller/TeacherController.java
@@ -1,13 +1,49 @@
 package org.demo.academicsystem.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.demo.academicsystem.dto.teacher.TeacherRequest;
+import org.demo.academicsystem.dto.teacher.TeacherResponse;
+import org.demo.academicsystem.service.TeacherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/teachers")
 @RequiredArgsConstructor
 @Tag(name = "Teachers")
 public class TeacherController {
+    private final TeacherService teacherService;
+
+    @GetMapping
+    public ResponseEntity<List<TeacherResponse>> getAllTeachers() {
+        return ResponseEntity.ok(teacherService.getAllTeachers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeacherResponse> getTeacherById(@PathVariable @Valid Long id) {
+        return ResponseEntity.ok(teacherService.getTeacherById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TeacherResponse> createAssignment(@RequestBody @Valid TeacherRequest teacher) {
+        return ResponseEntity.ok(teacherService.createTeacher(teacher));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TeacherResponse> updateAssignment(
+            @PathVariable @Valid Long id,
+            @RequestBody @Valid TeacherRequest teacher
+    ) {
+        return ResponseEntity.ok(teacherService.updateTeacher(id, teacher));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeacher(@PathVariable @Valid Long id) {
+        teacherService.deleteTeacher(id);
+        return ResponseEntity.noContent().build();
+    }
 }
