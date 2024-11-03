7f54187e16a6420c61ec1f0cebdbfdfd3515aaa2
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 12:47:36 -0600

    feature: add getAssignmentById method to AssignmentController with validation

--- src/main/java/org/demo/academicsystem/controller/AssignmentController.java
+++ src/main/java/org/demo/academicsystem/controller/AssignmentController.java
@@ -1,10 +1,12 @@
 package org.demo.academicsystem.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.demo.academicsystem.dto.assignment.AssignmentResponse;
 import org.demo.academicsystem.service.AssignmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,5 +23,10 @@ public class AssignmentController {
     @GetMapping
     public ResponseEntity<List<AssignmentResponse>> getAllAssignments() {
         return ResponseEntity.ok(assignmentService.getAllAssignments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AssignmentResponse> getAssignmentById(@PathVariable @Valid Long id) {
+        return ResponseEntity.ok(assignmentService.getAssignmentById(id));
     }
 }
