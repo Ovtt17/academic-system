68147e93eb236a89cb1ff2f4f6b89c0bc1679d05
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 12:20:45 -0600

    feature: add AssignmentServiceImpl

---
+++ src/main/java/org/demo/academicsystem/service/impl/AssignmentServiceImpl.java
@@ -0,0 +1,46 @@
+package org.demo.academicsystem.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.demo.academicsystem.dto.assignment.AssignmentRequest;
+import org.demo.academicsystem.dto.assignment.AssignmentResponse;
+import org.demo.academicsystem.entity.Assignment;
+import org.demo.academicsystem.mapper.AssignmentMapper;
+import org.demo.academicsystem.repository.AssignmentRepository;
+import org.demo.academicsystem.service.AssignmentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AssignmentServiceImpl implements AssignmentService {
+
+    private final AssignmentRepository assignmentRepository;
+    private final AssignmentMapper assignmentMapper;
+
+    @Override
+    public List<AssignmentResponse> getAllAssignments() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<AssignmentResponse> getAssignmentById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public AssignmentResponse createAssignment(AssignmentRequest assignment) {
+        return null;
+    }
+
+    @Override
+    public AssignmentResponse updateAssignment(Long id, AssignmentRequest assignment) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteAssigment(Long id) {
+        return false;
+    }
+}
