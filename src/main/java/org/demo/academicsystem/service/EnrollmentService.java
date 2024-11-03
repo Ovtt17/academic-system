6e600a033e399be7010cd11ccaa088bc9df2c3d2
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 14:05:19 -0600

    feature: implement EnrollmentService and EnrollmentServiceImpl for managing enrollments

---
+++ src/main/java/org/demo/academicsystem/service/EnrollmentService.java
@@ -0,0 +1,7 @@
+package org.demo.academicsystem.service;
+
+import org.demo.academicsystem.dto.enrollment.EnrollmentRequest;
+import org.demo.academicsystem.dto.enrollment.EnrollmentResponse;
+
+public interface EnrollmentService extends GenericService<EnrollmentRequest, EnrollmentResponse, Long>{
+}
