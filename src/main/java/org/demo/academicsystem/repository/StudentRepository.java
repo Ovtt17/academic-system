b5408e5d9b3e58e72f1676e6ea20b45bbdd668af
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 18:17:06 -0600

    feature: add method to retrieve top 10 students by teacher's average grade

--- src/main/java/org/demo/academicsystem/repository/StudentRepository.java
+++ src/main/java/org/demo/academicsystem/repository/StudentRepository.java
@@ -2,6 +2,18 @@ package org.demo.academicsystem.repository;
 
 import org.demo.academicsystem.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "SELECT s " +
+            "FROM Student s " +
+            "JOIN s.grades g " +
+            "WHERE g.teacher.id = :teacherId " +
+            "GROUP BY s.id " +
+            "ORDER BY AVG(g.grade) DESC " +
+            "LIMIT 10 ", nativeQuery = true)
+    List<Student> findTop10StudentsByTeacherIdOrderByAverageGradeDesc(@Param("teacherId") Long teacherId);
 }
