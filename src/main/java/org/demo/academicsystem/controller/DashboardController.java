e7334ad6cc4a731a8298a7a6e1ab4566aac09b1f
Ovett Mora <ovettmora17@gmail.com>
2024-11-03 19:32:21 -0600

    feature: add getDashboardData method to DashboardController for retrieving dashboard data

--- src/main/java/org/demo/academicsystem/controller/DashboardController.java
+++ src/main/java/org/demo/academicsystem/controller/DashboardController.java
@@ -2,6 +2,9 @@ package org.demo.academicsystem.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.demo.academicsystem.dto.dashboard.DashboardData;
+import org.demo.academicsystem.service.DashboardService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "Dashboard", description = "Dashboard API")
 public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public DashboardData getDashboardData(Long teacherId) {
+        return dashboardService.getDashboardData(teacherId);
+    }
 }
