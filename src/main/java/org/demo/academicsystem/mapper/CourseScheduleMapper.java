6f547bf3999c47d175588f06ed5dc34315224686
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 16:23:23 -0600

    feat: refactor mappers to use constructor injection and simplify entity mapping

--- src/main/java/org/demo/academicsystem/mapper/CourseScheduleMapper.java
+++ src/main/java/org/demo/academicsystem/mapper/CourseScheduleMapper.java
@@ -2,23 +2,25 @@ package org.demo.academicsystem.mapper;
 
 import org.demo.academicsystem.dto.courseSchedule.CourseScheduleRequest;
 import org.demo.academicsystem.dto.courseSchedule.CourseScheduleResponse;
-import org.demo.academicsystem.entity.Course;
 import org.demo.academicsystem.entity.CourseSchedule;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
 @Component
-public interface CourseScheduleMapper {
+public class CourseScheduleMapper {
+    public CourseSchedule toEntity(CourseScheduleRequest request) {
+        return CourseSchedule.builder()
+                .day(request.day())
+                .startTime(request.startTime())
+                .endTime(request.endTime())
+                .build();
+    }
 
-    @Mapping(target = "course", expression = "java(mapCourseIdToCourse(request.courseId()))")
-    CourseSchedule toEntity(CourseScheduleRequest request);
-    CourseScheduleResponse toResponse(CourseSchedule schedule);
-
-    default Course mapCourseIdToCourse(Long courseId) {
-        return Course.builder()
-                .id(courseId)
+    public CourseScheduleResponse toResponse(CourseSchedule courseSchedule) {
+        return CourseScheduleResponse.builder()
+                .id(courseSchedule.getId())
+                .day(courseSchedule.getDay())
+                .startTime(courseSchedule.getStartTime())
+                .endTime(courseSchedule.getEndTime())
                 .build();
     }
 }
