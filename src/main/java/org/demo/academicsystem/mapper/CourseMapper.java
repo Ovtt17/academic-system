00d42a22131acd6276a9d4b2a7680228ebee0798
Ovett Mora <ovettmora17@gmail.com>
2024-11-04 22:38:36 -0600

    refactor: simplify CourseMapper by removing studentIds mapping and related method

--- src/main/java/org/demo/academicsystem/mapper/CourseMapper.java
+++ src/main/java/org/demo/academicsystem/mapper/CourseMapper.java
@@ -3,10 +3,7 @@ package org.demo.academicsystem.mapper;
 import org.demo.academicsystem.dto.course.CourseRequest;
 import org.demo.academicsystem.dto.course.CourseResponse;
 import org.demo.academicsystem.dto.courseSchedule.CourseScheduleRequest;
-import org.demo.academicsystem.entity.Course;
-import org.demo.academicsystem.entity.CourseSchedule;
-import org.demo.academicsystem.entity.Student;
-import org.demo.academicsystem.entity.Teacher;
+import org.demo.academicsystem.entity.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
@@ -20,20 +17,11 @@ public interface CourseMapper {
 
     CourseScheduleMapper courseScheduleMapper = new CourseScheduleMapperImpl();
 
-    @Mapping(target = "students", expression = "java(mapStudentIdsToStudents(request.studentIds()))")
     @Mapping(target = "teacher", expression = "java(mapTeacherIdToTeacher(request.teacherId()))")
     @Mapping(target = "schedules", expression = "java(mapSchedules(request.schedules()))")
     Course toEntity(CourseRequest request);
 
     CourseResponse toResponse(Course course);
-
-    default List<Student> mapStudentIdsToStudents(List<Long> studentIds) {
-        return studentIds.stream()
-                .map(id -> Student.builder()
-                        .id(id)
-                        .build())
-                .collect(Collectors.toList());
-    }
 
     default Teacher mapTeacherIdToTeacher(Long teacherId) {
         return Teacher.builder()
