27cc338d0068d3a82508aa5ff67da4d9813da2a9
Ovett Mora <ovettmora17@gmail.com>
2024-11-04 22:51:33 -0600

    refactor: remove students field from CourseResponse

--- src/main/java/org/demo/academicsystem/dto/course/CourseResponse.java
+++ src/main/java/org/demo/academicsystem/dto/course/CourseResponse.java
@@ -2,7 +2,6 @@ package org.demo.academicsystem.dto.course;
 
 import org.demo.academicsystem.dto.assignment.AssignmentResponse;
 import org.demo.academicsystem.dto.courseSchedule.CourseScheduleResponse;
-import org.demo.academicsystem.dto.student.StudentResponse;
 import org.demo.academicsystem.dto.teacher.TeacherResponse;
 
 import java.util.List;
@@ -14,7 +13,6 @@ public record CourseResponse(
         String semester,
         List<CourseScheduleResponse> schedules,
         List<AssignmentResponse> assignments,
-        List<StudentResponse> students,
         TeacherResponse teacher
 ) {
 }
