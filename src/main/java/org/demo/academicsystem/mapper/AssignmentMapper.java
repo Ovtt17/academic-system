47629883429d60e9091394e9710f46f8125cb7b7
Ovett Mora <ovettmora17@gmail.com>
2024-11-05 11:01:36 -0600

    feat: refactor AssignmentMapper to include SubmissionService and map totalSubmissions

--- src/main/java/org/demo/academicsystem/mapper/AssignmentMapper.java
+++ src/main/java/org/demo/academicsystem/mapper/AssignmentMapper.java
@@ -4,20 +4,34 @@ import org.demo.academicsystem.dto.assignment.AssignmentRequest;
 import org.demo.academicsystem.dto.assignment.AssignmentResponse;
 import org.demo.academicsystem.entity.Assignment;
 import org.demo.academicsystem.entity.Course;
+import org.demo.academicsystem.service.SubmissionService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 @Component
-public interface AssignmentMapper {
-    @Mapping(target = "course", expression = "java(mapCourseIdToCourse(request.courseId()))")
-    Assignment toEntity(AssignmentRequest request);
-    AssignmentResponse toResponse(Assignment assignment);
+public abstract class AssignmentMapper {
 
-    default Course mapCourseIdToCourse(Long courseId) {
+    @Autowired
+    private SubmissionService submissionService;
+
+    @Mapping(target = "course", expression = "java(mapCourseIdToCourse(request.courseId()))")
+    public abstract Assignment toEntity(AssignmentRequest request);
+
+    @Mapping(target = "totalSubmissions", source = "id", qualifiedByName = "mapTotalSubmissions")
+    public abstract AssignmentResponse toResponse(Assignment assignment);
+
+    protected  Course mapCourseIdToCourse(Long courseId) {
         return Course.builder()
                 .id(courseId)
                 .build();
+    }
+
+    @Named("mapTotalSubmissions")
+    protected Long mapTotalSubmissions(Long assignmentId) {
+        return submissionService.countByAssignmentId(assignmentId);
     }
 }
