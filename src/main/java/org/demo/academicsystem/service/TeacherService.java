423986eb605970b700b58fd4ce6e10ee1d4b0b17
Ovett Mora <ovettmora17@gmail.com>
2025-03-22 00:01:55 -0600

    refactor: Add getTeacherByEmail method to TeacherService interface

--- src/main/java/org/demo/academicsystem/service/TeacherService.java
+++ src/main/java/org/demo/academicsystem/service/TeacherService.java
@@ -2,6 +2,8 @@ package org.demo.academicsystem.service;
 
 import org.demo.academicsystem.dto.teacher.TeacherRequest;
 import org.demo.academicsystem.dto.teacher.TeacherResponse;
+import org.demo.academicsystem.entity.Teacher;
 
 public interface TeacherService extends GenericService<TeacherRequest, TeacherResponse, Long> {
+    Teacher getTeacherByEmail(String email);
 }
