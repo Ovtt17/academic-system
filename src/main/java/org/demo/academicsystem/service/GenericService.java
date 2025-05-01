f4bf636073226efd5f06650ec1f0f0ffd2848fbc
Ovett Mora <ovettmora17@gmail.com>
2025-04-30 20:04:14 -0600

    refactor: Remove @Transactional annotations from GenericService interface methods

--- src/main/java/org/demo/academicsystem/service/GenericService.java
+++ src/main/java/org/demo/academicsystem/service/GenericService.java
@@ -1,16 +1,11 @@
 package org.demo.academicsystem.service;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 public interface GenericService<Request, Response, ID> {
     List<Response> getAll();
     Response getById(ID id);
-    @Transactional
     Response create(Request request);
-    @Transactional
     Response update(ID id, Request request);
-    @Transactional
     void delete(ID id);
 }
